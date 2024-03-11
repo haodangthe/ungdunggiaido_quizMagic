ab1357ad0f612e511f1de7c6d7a6493ad7e22788
HoangLong <hoanglong180903@gmail.com>
2024-03-11 21:55:09 +0700

    Hoàn thành giao diện xếp hạng đổi thưởng, user setting.

---
+++ app/src/main/java/com/example/ungdunggiaido_quizmagic/fragment/XepHangFragment.java
@@ -0,0 +1,32 @@
+package com.example.ungdunggiaido_quizmagic.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ungdunggiaido_quizmagic.R;
+
+
+public class XepHangFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_xep_hang, container, false);
+        return view;
+    }
+
+
+}
